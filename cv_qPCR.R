5d50a01e0e3d4b64dcd5283a7729f3392c7c3b7c
Siskasam <509332@muni.cz>
2025-03-07 13:48:02 +0100

    sloupce sumarizovány

--- cv_qPCR.R
+++ cv_qPCR.R
@@ -1,15 +1,30 @@
 library("dplyr")
+#install.packages("xlsx")
+library("xlsx")
+library("readxl")
+Sys.setenv(JAVA_HOME='C:\\Program Files (x86)\\Java\\jre1.8.0_271')
+
+library("rJava")
+
+library(xlsx)
 
 #############################
 ## data qPCR ----
 
 # Nactete data ze souboru data_examples.xlsx, list "qPCR". 
-
+read.xlsx("data_examples.xlsx", sheetName="qPCR")
+read_excel("data_examples.xlsx", sheet = "qPCR")
 
 # Spocitejte sumarizace pro vsechny sloupce. 
+qPCR <- read_excel("data_examples.xlsx", sheet = "qPCR")
+View(qPCR)
+
+qPCR %>% 
+  summary()
 
 # Prevedte sloupce na faktory, kde to dava smysl. 
-
+qPCR %>% 
+  as.factor(Replicate)
 
 # Vypiste pouze radky s kondici wt 
 # Vypiste pouze radky s kondici wt pro geny Fzd2 a Wnt9a
