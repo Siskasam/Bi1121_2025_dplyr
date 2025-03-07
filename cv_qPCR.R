8f827d3dbf0b4ce1df7c42a896b187ffca07b0f3
Siskasam <509332@muni.cz>
2025-03-07 12:49:10 +0100

    nový soubor

---
+++ cv_qPCR.R
@@ -0,0 +1,25 @@
+library("dplyr")
+
+#############################
+## data qPCR ----
+
+# Nactete data ze souboru data_examples.xlsx, list "qPCR". 
+
+
+# Spocitejte sumarizace pro vsechny sloupce. 
+
+# Prevedte sloupce na faktory, kde to dava smysl. 
+
+
+# Vypiste pouze radky s kondici wt 
+# Vypiste pouze radky s kondici wt pro geny Fzd2 a Wnt9a
+
+# Spocitejte prumery pro jednotlive kondice u jednotlivych genu. 
+
+# Prumery vypiste v poradi genu Prickle1, Fzd2, Wnt9a a kondice wt, #1, #2, #3. 
+
+# ÚKOL NAVÍC: Muzete se pokusit vykreslit jednoduchy graf s prumery. 
+
+# Vyjadrete expresi relativne vuci hodnote wt v kazdem replikatu. 
+
+# Pro tyto hodnoty vyjadrete prumer. 
